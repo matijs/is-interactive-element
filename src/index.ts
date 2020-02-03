4ed81a96e18dc2b4ed1db169b6f8faaac2338e1e
Matijs Brinkhuis <matijs@gmail.com>
2020-02-03 08:06:34 +0100

    Convert to TypeScript
    Convert the code to TypeScript.
    
    Move code to ./src and output to ./dist
    
    Replace Mocha with Jest.

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { isInteractiveElement } from './is-interactive-element';
---
+++ src/is-interactive-element.ts
@@ -0,0 +1,48 @@
+const isInputHidden = (input: HTMLInputElement) => input.type === 'hidden';
+
+export function isInteractiveElement(element: HTMLElement) {
+    const { nodeName } = element;
+
+    if (
+        [
+            'BUTTON',
+            'DETAILS',
+            'EMBED',
+            'IFRAME',
+            'KEYGEN',
+            'LABEL',
+            'SELECT',
+            'TEXTAREA',
+        ].includes(nodeName)
+    ) {
+        return true;
+    }
+
+    if (nodeName === 'A' && element.hasAttribute('href')) {
+        return true;
+    }
+
+    if (element instanceof HTMLInputElement && !isInputHidden(element)) {
+        return true;
+    }
+
+    if (
+        ['AUDIO', 'VIDEO'].includes(nodeName) &&
+        element.hasAttribute('controls')
+    ) {
+        return true;
+    }
+
+    if (
+        ['IMG', 'OBJECT'].includes(nodeName) &&
+        element.hasAttribute('usemap')
+    ) {
+        return true;
+    }
+
+    if (element.hasAttribute('tabindex') && element.tabIndex > -1) {
+        return true;
+    }
+
+    return false;
+}
